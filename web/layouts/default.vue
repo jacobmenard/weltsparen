ffb0848eff43ba8eed6e2ea06de46833a9cb17f8
Menard <jemenard082713@gmail.com>
2025-07-22 13:25:34 +0800

    frontend additional setup

---
+++ web/layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="main-container">
+
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
