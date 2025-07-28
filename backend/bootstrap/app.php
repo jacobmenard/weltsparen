7a35a23b927c6215915b99b5796bb8067406a9ab
JRos <joseci.rosales@gmail.com>
2025-07-28 20:45:39 +0800

    Added api throttle

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->group('api', [
             EnsureFrontendRequestsAreStateful::class,
+            ThrottleRequests::class.':' . env('APP_DEFAULT_API_THROTTLE', '60,1'),
             SubstituteBindings::class,
         ]);
     })
